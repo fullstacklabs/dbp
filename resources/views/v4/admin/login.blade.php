9bc5a27e3f8ee8cdd100ff23dc27310c6ea44183
jramr7 <jramirez@fullstacklabs.co>
2020-11-23 13:52:18 -0500

    FCBH-2608 user login for admin key management

--- resources/views/v4/admin/login.blade.php
+++ resources/views/v4/admin/login.blade.php
@@ -4,24 +4,35 @@
 @endsection
 
 @section('content')
-<form method="POST" action="{{ route('admin.login') }}">
-    <h2>Authentication Required</h2>
-    <p>The server requires an username and password</p>
-    @if($errors->has('auth.failed'))
-    <div>{{ $errors->first('auth.failed') }}</div>
-    @endif
-    <div>
-        <label for="email">Username</label>
-        <input id="email" type="text" autocomplete="email" name="email" value="{{ old('email') }}" required autofocus placeholder="user@email.com">
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input id="password" type="password" name="password" required placeholder="Password">
-    </div>
-    <div>
-        <button type="submit">Log In</button>
-        <a href="{{ route('welcome') }}">Cancel</a>
-    </div>
-    <input name="_token" value="{{ csrf_token() }}" type="hidden" />
-</form>
+<div class="card">
+    <form method="POST" action="{{ route('admin.login') }}">
+        <p class="card-header">Authentication Required</p>
+        <p class = "card-subtitle">The server https://dbp.com/admin requires a username 
+            <br> and password</p>
+            @if($errors->has('auth.failed'))
+                <div>{{ $errors->first('auth.failed') }}</div>
+            @endif
+        <div class="input-row">
+            <label class="login-label" for="email">Username</label>
+            <div class="input-icons"> 
+                <i class="fa fa-user icon"></i> 
+                <input class="login-input" id="email" type="text" autocomplete="email" name="email" value="{{ old('email') }}" required autofocus placeholder="user@email.com">
+            </div>
+        </div> 
+        <div class="input-row">
+            <label class="login-label" for="password">Password</label>
+            <div class="input-icons"> 
+                <i class="fa fa-lock icon"></i> 
+                <input class="login-input" id="password" type="password" name="password" required placeholder="Please type your password...">
+            </div>
+        </div> 
+            
+        <div class="row login-btns">
+            <button class="btn btn-success" type="submit">Log In</button>
+            <a class="btn btn-cancel" href="{{ route('welcome') }}">Cancel</a>
+        </div>
+        <input name="_token" value="{{ csrf_token() }}" type="hidden" />
+    </form>
+</div>
+
 @endsection
--- resources/views/v4/layouts/partials/nav.blade.php
+++ resources/views/v4/layouts/partials/nav.blade.php
@@ -1,13 +1,16 @@
-<nav>
-    <div>
+<nav class="row header-nav {{ Auth::user() ? 'logged-header' : ''  }}">
+    <div class="header-img">
+        @isset($image) <img src="{{ $image }}"> @endisset
+    </div>
+    @isset($image_text) <h4 class="image-text"> {{ $image_text }} </h4> @endisset
+
+    @if(Auth::user())
+    <div class="row user-header">
+        <i class="fa fa-user circle-icon" aria-hidden="true"></i>
         <div>
-            Digital Bible Platform
-        </div>
-        <div>
-            @if(Auth::user())
-            <div>{{ Auth::user()->email }}</div>
-            <a href="{{ route('admin.logout') }}">Log Out</a>
-            @endif
+          <div class='user-email'>{{ Auth::user()->email }}</div>
+          <a class="logout-a" href="{{ route('admin.logout') }}">Log Out</a>
         </div>
     </div>
+    @endif
 </nav>
