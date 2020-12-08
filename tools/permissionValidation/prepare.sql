8866e481117ca1f1dcffbad8856f0f7b83b7a7b5
Brad Flood <bflood@keyholesoftware.com>
2020-12-08 09:27:05 -0600

    add database name;ignore local file

--- tools/permissionValidation/prepare.sql
+++ tools/permissionValidation/prepare.sql
@@ -2,31 +2,31 @@
  script to prepare database for test execution
  */
 insert into users (v2_id, name, token,password) values ('909090909', 'testuser', 'token', 'password');
-select id from users where v2_id='909090909' # returns 1255629  
+select id from users where v2_id='909090909' # returns 1255629. modify below to match 
 
-insert into user_keys (user_id, `key`) values (1255629, 'testkey_none') ; 
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-101') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-102') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-103') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-111') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-113') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-115') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-121') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-123') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-125') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-131') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-133') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-135') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-141') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-143') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-145') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-153') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-155') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-163') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-165') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-173') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-175') ;
-insert into user_keys (user_id, `key`) values (1255629, 'testkey-183') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey_none') ; 
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-101') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-102') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-103') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-111') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-113') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-115') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-121') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-123') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-125') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-131') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-133') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-135') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-141') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-143') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-145') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-153') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-155') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-163') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-165') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-173') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-175') ;
+insert into user_keys (dbp_users.user_id, `key`) values (1255629, 'testkey-183') ;
 
 
 insert into access_group_api_keys (access_group_id, key_id)
