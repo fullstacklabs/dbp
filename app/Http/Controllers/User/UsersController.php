ca7267e329d446e8eb17f3cf42d66a290807abe4
jramr7 <jramirez@fullstacklabs.co>
2021-01-25 09:29:41 -0500

    FCBH-3126 login validator remove and add log with email

--- app/Http/Controllers/User/UsersController.php
+++ app/Http/Controllers/User/UsersController.php
@@ -301,7 +301,7 @@ class UsersController extends APIController
         
         // if exists update the provider_user_id (For now throw error to newrelic)
         if ($existing_account && ($provider_user_id !== $existing_account->provider_user_id)) {
-            $provider_error_message = 'Login error on request' . json_encode($request) . ' with different provider_user_id ' . $provider_user_id .
+            $provider_error_message = 'Login error on email' . json_encode($request->email) . ' with different provider_user_id ' . $provider_user_id .
                                       ' account data:' . json_encode($existing_account);
             Log::error($provider_error_message);
         }
