ff660d1c791155c954a3c6a3487f9683dcb846fd
jramr7 <jramirez@fullstacklabs.co>
2021-01-26 15:23:49 -0500

    FCBH-3123 remove user for testing

--- resources/views/api_key/dashboard.blade.php
+++ resources/views/api_key/dashboard.blade.php
@@ -221,7 +221,7 @@
 @endsection
 
 @section('content')
-<!-- @if($user->roles->where('slug','admin')->first()) -->
+<!-- @#if($user->roles->where('slug','admin')->first()) -->
 <h2 class="dashboard-title">Key Management</h2>
 <div class="dashboard-card">
     <div class="key-filter">
@@ -277,9 +277,9 @@
     <p>Empty Results</p>
     @endif
 </div>
-<!--@else
+<!--@#else
     <p>You are not an admin</p>
-@endif -->
+@#endif -->
 
 <div class="pagination">
     {{$key_requests->appends(['state' => $state,'search' => $search])->links()}}
