e773de67bc34ed7a6dc65a12dba6f817a1f89cfb
jramr7 <jramirez@fullstacklabs.co>
2021-06-10 16:06:41 -0500

    FCBHDBP-126 fix cache params on bibles all endpoint

--- routes/api.php
+++ routes/api.php
@@ -60,7 +60,6 @@ Route::name('v4_bible.one')->get(
     'Bible\BiblesController@show'
 ); // see note in Postman. the content is suspect
 Route::name('v4_bible.all')
-    ->middleware('BibleIsBackwardCompatibility')
     ->get('bibles', 'Bible\BiblesController@index'); // used
 Route::name('v4_bible.copyright')->get(
     'bibles/{bible_id}/copyright',
