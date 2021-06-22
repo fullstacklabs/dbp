757e96229aa0088b33dd0413fe086711edc9a54c
jramr7 <jramirez@fullstacklabs.co>
2021-06-21 20:24:15 -0500

    FCBHDBP-3 use correct data on annotations and check annotation exists

--- app/Console/Commands/syncLiveBibleIsNotes.php
+++ app/Console/Commands/syncLiveBibleIsNotes.php
@@ -43,30 +43,29 @@ class syncLiveBibleIsNotes extends Command
 
         echo "\n" . Carbon::now() . ': liveBibleis to v4 notes sync started.';
         $chunk_size = config('settings.v2V4SyncChunkSize');
+        $syncFile = config('settings.bibleSyncFilePath');
+        $transition_bibles = convertCsvToArrayMap($syncFile);
 
         DB::connection($db_name)
             ->table('user_notes')
             ->where('created_at', '>=', $from_date)
-            ->orderBy('id')->chunk($chunk_size, function ($notes) use ($filesets) {
+            ->orderBy('id')->chunk($chunk_size, function ($notes) use ($filesets, $transition_bibles) {
                 $bible_ids = $notes->pluck('bible_id')->reduce(function ($carry, $item) use ($filesets) {
-                    if (!isset($carry[$item])) {
-                        $fileset = getFilesetFromDamId($item, true, $filesets);
-                        if ($fileset) {
-                            $carry[$item] = $fileset;
-                            $this->bible_ids[$item] = $fileset;
-                        }
+                    // checks if filesets actually exist for the given bible
+                    $fileset = getFilesetFromDamId($item, true, $filesets);
+                    if ($fileset) {
+                        $carry[$item] = $fileset;
+                        $this->bible_ids[$item] = $fileset;
                     }
                     return $carry;
                 }, []);
 
-                $notes = $notes->filter(function ($note) use ($bible_ids) {
-                    return validateLiveBibleIsAnnotation($note, $bible_ids);
-                });
-
-                $notes = $notes->map(function ($note) use ($bible_ids) {
+                $notes = $notes->map(function ($note) use ($bible_ids, $transition_bibles) {
+                    $bible_id = $note->bible_id;
+                    $v4_bible_id = array_key_exists($bible_id, $transition_bibles) ? $transition_bibles[$bible_id] : $bible_id;
                     return [
                         'user_id'     => $note->user_id,
-                        'bible_id'    => $bible_ids[$note->bible_id]->bible->first()->id,
+                        'bible_id'    => $bible_ids[$v4_bible_id]->bible->first()->id,
                         'book_id'     => $note->book_id,
                         'notes'       => $note->notes,
                         'chapter'     => $note->chapter,
@@ -77,8 +76,20 @@ class syncLiveBibleIsNotes extends Command
                     ];
                 });
 
-                $chunks = $notes->chunk(5000);
+                $notes = $notes->filter(function ($note) use ($bible_ids) {
+                    $note_exists = Note::where([
+                        'user_id'       => $note['user_id'], 
+                        'bible_id'      => $note['bible_id'],
+                        'book_id'       => $note['book_id'],
+                        'chapter'       => $note['chapter'],
+                        'verse_start'   => $note['verse_start'],
+                        'verse_end'     => $note['verse_end'],
+                        'notes'         => $note['notes'],
+                    ])->first();
+                    return validateLiveBibleIsAnnotation($note, $bible_ids, $note_exists);
+                });
 
+                $chunks = $notes->chunk(5000);
                 foreach ($chunks as $chunk) {
                     Note::insert($chunk->toArray());
                 }
