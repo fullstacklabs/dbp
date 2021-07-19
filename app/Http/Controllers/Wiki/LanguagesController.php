a4b9cefefdd3448bb681b8cd9251884408fd9fc1
jramr7 <jramirez@fullstacklabs.co>
2021-07-19 11:30:15 -0500

    FCBHDBP-146 remove heavyCalls cache and add checks on helpers to avoid 500

--- app/Http/Controllers/Wiki/LanguagesController.php
+++ app/Http/Controllers/Wiki/LanguagesController.php
@@ -116,7 +116,7 @@ class LanguagesController extends APIController
         $order = $country ? 'country_population.population' : 'ifnull(current_translation.name, languages.name)';
         $order_dir = $country ? 'desc' : 'asc';
         $select_country_population = $country ? 'country_population.population' : 'null';
-        $languages = cacheRememberForHeavyCalls('languages_all', $cache_params, now()->addDay(), function () use ($country, $include_translations, $code, $name, $access_control, $order, $order_dir, $select_country_population, $limit, $page, $asset_filter) {
+        $languages = cacheRemember('languages_all', $cache_params, now()->addDay(), function () use ($country, $include_translations, $code, $name, $access_control, $order, $order_dir, $select_country_population, $limit, $page, $asset_filter) {
             $languages = Language::includeCurrentTranslation()
                 ->includeAutonymTranslation()
                 ->includeExtraLanguages(arrayToCommaSeparatedValues($access_control->hashes), $asset_filter)
