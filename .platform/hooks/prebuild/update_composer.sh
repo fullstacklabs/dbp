abfd90a4729ea6a03b10683828146863941d899f
Brad Flood <bflood@keyholesoftware.com>
2021-07-22 10:45:38 -0700

    config

--- .platform/hooks/prebuild/update_composer.sh
+++ .platform/hooks/prebuild/update_composer.sh
@@ -3,9 +3,10 @@
 # Update Composer binary.
 
 #export COMPOSER_HOME=/root
-export COMPOSER_MEMORY_LIMIT=-1
+#export COMPOSER_MEMORY_LIMIT=-1
 
-#sudo COMPOSER_MEMORY_LIMIT=-1 /usr/bin/composer.phar self-update
-echo "prebuild / update_composer.sh -- command commented out"
-echo "COMPOSER_MEMORY_LIMIT = " $COMPOSER_MEMORY_LIMIT
-touch /tmp/touch-from-update-composer
+#echo "prebuild / update_composer.sh -- command commented out"
+#echo "COMPOSER_MEMORY_LIMIT = " $COMPOSER_MEMORY_LIMIT
+
+
+# this is  probably not needed, and can be removed once everything else is working
