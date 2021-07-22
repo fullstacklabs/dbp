da240056a780a41f67a2bfbffae33e9c0493004d
Brad Flood <bflood@keyholesoftware.com>
2021-07-22 09:34:23 -0700

    config

---
+++ .platform/hooks/prebuild/env.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mv /tmp/.env /var/app/staging/.env
+mv /tmp/pub.pem /var/app/staging/pub.pem
+mv /tmp/priv.pem /var/app/staging/priv.pem
--- .platform/hooks/prebuild/update_composer.sh
+++ .platform/hooks/prebuild/update_composer.sh
@@ -8,3 +8,4 @@ export COMPOSER_MEMORY_LIMIT=-1
 #sudo COMPOSER_MEMORY_LIMIT=-1 /usr/bin/composer.phar self-update
 echo "prebuild / update_composer.sh -- command commented out"
 echo "COMPOSER_MEMORY_LIMIT = " $COMPOSER_MEMORY_LIMIT
+touch /tmp/touch-from-update-composer
