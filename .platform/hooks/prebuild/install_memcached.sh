b653768674c6dd5e98161cf72f333ddca1e6eeb8
Brad Flood <bflood@keyholesoftware.com>
2021-07-28 16:39:44 -0700

    config

--- .platform/hooks/prebuild/install_memcached.sh
+++ .platform/hooks/prebuild/install_memcached.sh
@@ -9,3 +9,23 @@
 # Enabled by default since it's latest Laravel version's default driver.
 
 #sudo yum -y install php-redis
+
+
+# packages:
+#   yum:
+#     libmemcached-devel: []
+# commands:
+#   01_install_memcached:
+#     command: /usr/bin/yes 'no'| /usr/bin/pecl install memcached
+#     test: '! /usr/bin/pecl info memcached'
+#   02_remove_extension:
+#     command: /bin/sed -i -e '/extension="memcached.so"/d' /etc/php.ini
+#   03_create_conf:
+#     command: /bin/echo 'extension="memcached.so"' > /etc/php.d/41-memcached.ini
+
+    sudo yum install libmemcached-devel -y
+    sudo pecl channel-update pecl.php.net
+    /usr/bin/yes 'no'| /usr/bin/pecl install memcached
+    /bin/sed -i -e '/extension="memcached.so"/d' /etc/php.ini
+    /bin/echo 'extension="memcached.so"' > /etc/php.d/41-memcached.ini # there is already a 50-memcached.ini. may not need this
+
