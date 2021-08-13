ef06970882c24a7df9199e229b7dd178f23b799f
Brad Flood <bflood@keyholesoftware.com>
2021-08-12 20:14:09 -0700

    config

--- .platform/confighooks/postdeploy/01_echo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-echo "................file: platform/confighooks/postdeploy/echo.sh"
-
--- .platform/hooks/prebuild/install_memcached.sh
+++ .platform/hooks/prebuild/install_memcached.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
 
-# Install PhpRedis
-
-# Laravel uses by default PhpRedis, so the extension needs to be installed.
-# https://github.com/phpredis/phpredis
-
-# For Predis, this extension is not necessarily needed.
-# Enabled by default since it's latest Laravel version's default driver.
-
-#sudo yum -y install php-redis
-
-
-# packages:
-#   yum:
-#     libmemcached-devel: []
-# commands:
-#   01_install_memcached:
-#     command: /usr/bin/yes 'no'| /usr/bin/pecl install memcached
-#     test: '! /usr/bin/pecl info memcached'
-#   02_remove_extension:
-#     command: /bin/sed -i -e '/extension="memcached.so"/d' /etc/php.ini
-#   03_create_conf:
-#     command: /bin/echo 'extension="memcached.so"' > /etc/php.d/41-memcached.ini
-
-    sudo yum install libmemcached -y
-    sudo pecl channel-update pecl.php.net
-    /usr/bin/yes 'no'| /usr/bin/pecl install memcached | true
-    #/bin/sed -i -e '/extension="memcached.so"/d' /etc/php.ini
-    #/bin/sed -i 's/;memcached.serializer = "igbinary"/memcached.serializer = "igbinary"/g' /etc/php.d/50-memcached.ini    
+sudo yum install libmemcached -y
+sudo pecl channel-update pecl.php.net
+/usr/bin/yes 'no'| /usr/bin/pecl install memcached | true
+/bin/sed -i 's/;memcached.serializer = "igbinary"/memcached.serializer = "igbinary"/g' /etc/php.d/50-memcached.ini    
     
