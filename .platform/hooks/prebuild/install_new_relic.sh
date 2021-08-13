ba832e0150f723064f04b8b10c034bdb13ca698e
Brad Flood <bflood@keyholesoftware.com>
2021-08-12 17:14:51 -0700

    config

--- .platform/hooks/prebuild/install_new_relic.sh
+++ .platform/hooks/prebuild/install_new_relic.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# current working directory will have .env file from S3
-source .env && \
+source /var/app/staging/.env && \
 rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
 yum install newrelic-php5 -y && \
 export NR_INSTALL_SILENT=true && \
