cb0e29e24827544d34198732f4adf686f7cfa037
Brad Flood <bflood@keyholesoftware.com>
2021-08-16 09:24:44 -0700

    config

--- .platform/hooks/prebuild/install_memcached.sh
+++ .platform/hooks/prebuild/install_memcached.sh
@@ -3,5 +3,4 @@
 sudo yum install libmemcached -y
 sudo pecl channel-update pecl.php.net
 /usr/bin/yes 'no'| /usr/bin/pecl install -D 'enable-memcached-igbinary="yes"' memcached | true
-/bin/sed -i 's/;memcached.serializer = igbinary/memcached.serializer = igbinary/g' /etc/php.d/50-memcached.ini    
     
