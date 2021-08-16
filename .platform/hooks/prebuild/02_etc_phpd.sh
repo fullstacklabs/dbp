cfd5f6410fbfb0ca1e888dcd4fa44caa2b013f20
Brad Flood <bflood@keyholesoftware.com>
2021-08-16 11:05:13 -0700

    config

--- .platform/hooks/prebuild/02_etc_phpd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-STAGING=`/opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir`
-cp $STAGING/.platform/files/etc/php.d/* /etc/php.d
--- .platform/hooks/prebuild/install_memcached.sh
+++ .platform/hooks/prebuild/install_memcached.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo yum install libmemcached -y
+sudo yum install libmemcached-devel -y
 sudo pecl channel-update pecl.php.net
+/usr/bin/yes 'no'| /usr/bin/pecl install igbinary
 /usr/bin/yes 'no'| /usr/bin/pecl install -D 'enable-memcached-igbinary="yes"' memcached | true
     
