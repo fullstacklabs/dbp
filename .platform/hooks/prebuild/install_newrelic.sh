5ccbf02c5778e01451a7e394fe4b89cc89d3fc22
Brad Flood <bflood@keyholesoftware.com>
2021-08-26 11:03:43 -0500

    config

--- .platform/hooks/prebuild/install_newrelic.sh
+++ .platform/hooks/prebuild/install_newrelic.sh
@@ -4,7 +4,7 @@ whoami
 
 ls -la 
 rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
-yum install newrelic-php5
+yum install newrelic-php5 -y
 source /var/app/staging/.env
 NR_INSTALL_SILENT=true
 export NR_INSTALL_KEY; newrelic-install install
