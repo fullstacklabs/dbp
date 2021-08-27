3531955af581da25ef83ec63871ebf497422e68c
Brad Flood <bflood@keyholesoftware.com>
2021-08-27 17:21:26 -0500

    config

--- .platform/hooks/prebuild/install_memcached.sh
+++ .platform/hooks/prebuild/install_memcached.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+cp .platform/files/etc-phpd/40-igbinary.ini /etc/php.d
+#cp .platform/files/etc-phpd/40-memcache.ini /etc/php.d
+
+
+
+
 sudo yum install libmemcached-devel -y
 sudo pecl channel-update pecl.php.net
 /usr/bin/yes 'no'| /usr/bin/pecl install igbinary
