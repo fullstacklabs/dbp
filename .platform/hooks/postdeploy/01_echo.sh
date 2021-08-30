1a57af6223c770f088d89d2eb277b2b60689bb19
Brad Flood <bflood@keyholesoftware.com>
2021-08-30 17:37:32 -0500

    config

--- .platform/hooks/postdeploy/01_echo.sh
+++ .platform/hooks/postdeploy/01_echo.sh
@@ -6,5 +6,5 @@ ls -la /var/app/staging/.env | true
 ls -la /var/app/current/.env |true
 
 
-echo "APP_SERVER_NAME=$(curl http://169.254.169.254/latest/meta-data/instance-id)" >> /var/app/current/.env
+#echo "APP_SERVER_NAME=$(curl http://169.254.169.254/latest/meta-data/instance-id)" >> /var/app/current/.env
 echo "API_URL=$(/opt/elasticbeanstalk/bin/get-config environment -k API_URL)" >> /var/app/current/.env
