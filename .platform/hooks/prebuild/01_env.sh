32e6f626690544ae417cc961b6103b5dad5a1cce
Brad Flood <bflood@keyholesoftware.com>
2021-09-08 13:14:31 -0500

    config

--- .platform/hooks/prebuild/01_env.sh
+++ .platform/hooks/prebuild/01_env.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-ls -la /tmp
-
 cp /tmp/.env /var/app/staging/.env
 cp /tmp/pub.pem /var/app/staging/pub.pem
 cp /tmp/priv.pem /var/app/staging/priv.pem
 cp /tmp/priv.pem /var/app/staging/storage/app/priv.pem
-
-
-ls -la /var/app/staging
--- .platform/scripts/01_echo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-echo "................file: platform/scripts/echo.sh"
-
