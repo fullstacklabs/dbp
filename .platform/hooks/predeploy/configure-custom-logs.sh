f4b5d02e1bc8b7f63b45ba28eb2c39e394f9579c
Brad Flood <bflood@keyholesoftware.com>
2021-09-20 11:24:30 -0500

    config

--- .platform/hooks/predeploy/configure-custom-logs.sh
+++ .platform/hooks/predeploy/configure-custom-logs.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 # custom logs to map API key to Cloudwatch Signature
-#mkdir -p /var/log/cloudfront-api-key/rotated
 #cron
 cp .platform/files/etc/cron.hourly/cron.logrotate.elasticbeanstalk.cloudfront-api-key.conf /etc/cron.hourly
-#logrotate
+#logrotate to /var/log/rotated
 cp .platform/files/etc/logrotate.elasticbeanstalk.hourly/logrotate.elasticbeanstalk.cloudfront-api-key.conf /etc/logrotate.elasticbeanstalk.hourly
-#publish to s3
-cp .platform/files/opt/elasticbeanstalk/tasks/publishlogs.d/cloudfront-api-key.conf /opt/elasticbeanstalk/tasks/publishlogs.d 
+#publish to s3 is done via existing publishlogs config, which publishes everything from /var/log/rotated
