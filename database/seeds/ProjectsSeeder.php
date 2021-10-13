9d20a5edf831bae275909c99652230e1200a069a
Victor Gonzalez <victor@fullstacklabs.co>
2021-10-13 10:24:12 -0500

    FCBHDBP-203 It has replaced the fzaninotto/faker package by the new package: fakerphp/faker. Fixed the ProjectsSeeder file.

--- database/seeds/ProjectsSeeder.php
+++ database/seeds/ProjectsSeeder.php
@@ -1,4 +1,5 @@
 <?php
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
@@ -17,8 +18,9 @@ class ProjectsSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $dbp_users = config('database.connections.dbp_users.database');
         Project::create([
-            'id'              => unique_random('projects', 'id'),
+            'id'              => unique_random($dbp_users.'.projects', 'id'),
             'name'            => 'Digital Bible Platform',
             'url_avatar'      => $faker->url,
             'url_avatar_icon' => $faker->url,
@@ -30,7 +32,7 @@ class ProjectsSeeder extends Seeder
         $project_count = random_int(3, 5);
         while ($project_count > 0) {
             Project::create([
-                'id'              => $project_count,
+                'id'              => unique_random($dbp_users.'.projects', 'id'),
                 'name'            => $faker->company,
                 'url_avatar'      => $faker->url,
                 'url_avatar_icon' => $faker->url,
@@ -40,19 +42,21 @@ class ProjectsSeeder extends Seeder
             ]);
 
             ProjectOauthProvider::create([
+                'id'            => unique_random($dbp_users.'.project_oauth_providers', 'id'),
                 'name'          => 'facebook',
                 'project_id'    => $project_count,
-                'client_id'     => unique_random('project_oauth_providers', 'client_id'),
-                'client_secret' => unique_random('project_oauth_providers', 'client_secret'),
+                'client_id'     => unique_random($dbp_users.'.project_oauth_providers', 'client_id'),
+                'client_secret' => unique_random($dbp_users.'.project_oauth_providers', 'client_secret'),
                 'callback_url'  => 'https://dbp4.org/login/callback/facebook',
                 'description'   => (string) $faker->paragraph(),
             ]);
 
             ProjectOauthProvider::create([
+                'id'            => unique_random($dbp_users.'.project_oauth_providers', 'id'),
                 'name'          => 'google',
                 'project_id'    => $project_count,
-                'client_id'     => unique_random('project_oauth_providers', 'client_id'),
-                'client_secret' => unique_random('project_oauth_providers', 'client_secret'),
+                'client_id'     => unique_random($dbp_users.'.project_oauth_providers', 'client_id'),
+                'client_secret' => unique_random($dbp_users.'.project_oauth_providers', 'client_secret'),
                 'callback_url'  => 'https://dbp4.org/login/callback/google',
                 'description'   => (string) $faker->paragraph(),
             ]);
