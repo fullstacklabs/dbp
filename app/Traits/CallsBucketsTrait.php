21b3a42e87eb161f940c33492d2c96c65850b5ed
Victor Gonzalez <victor@fullstacklabs.co>
2021-11-09 22:58:40 -0500

    FCBHDBP-232 It has created a new method to sign an url but the cloudFrontClient is passed by parameter to avoid the external call to AWS for each signing request.

--- app/Traits/CallsBucketsTrait.php
+++ app/Traits/CallsBucketsTrait.php
@@ -38,6 +38,43 @@ trait CallsBucketsTrait
             return $security_token;
         }
     }
+    /**
+     * Method to sign a URL but the CloudFrontClient object should be passed by parameter
+     * to do an only one call to AWS
+     *
+     * @param CloudFrontClient $client
+     * @param string $file_path
+     * @param int $transaction
+     *
+     * @return string signed url
+     */
+    public function signedUrlUsingClient(CloudFrontClient $client, string $file_path, int $transaction): string
+    {
+        if (!$client) {
+            return null;
+        }
+
+        $cdn_server_url = config('services.cdn.server');
+        $request_array = [
+            'url'         => 'https://'. $cdn_server_url . '/' . $file_path . '?x-amz-transaction=' . $transaction,
+            'key_pair_id' => config('filesystems.disks.cloudfront.key'),
+            'private_key' => storage_path('app/' . config('filesystems.disks.cloudfront.secret')),
+            'expires'     => Carbon::now()->addDay()->timestamp,
+        ];
+
+        $signed_url = $client->getSignedUrl($request_array);
+        $query_parameters_string = parse_url($signed_url, PHP_URL_QUERY);
+        $query_parameters = [];
+        parse_str($query_parameters_string, $query_parameters);
+        $key = $this->getKey();
+
+        if (!empty($key)) {
+            $signature = isset($query_parameters['Signature']) ? $query_parameters['Signature'] : $signed_url;
+            \Log::channel('cloudfront_api_key')->notice($key . ' ' . $signature);
+        }
+
+        return $signed_url;
+    }
 
     public function signedUrl(string $file_path, $asset_id, int $transaction)
     {
