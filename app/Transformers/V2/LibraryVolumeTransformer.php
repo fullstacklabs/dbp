dedfbd783391f5a84f0a4b7b55a38a4dbedd4d80
Victor Gonzalez <victor@fullstacklabs.co>
2021-12-15 03:29:57 -0500

    FCBHDBP-233 The library volume endpoint has been update to filter by language family code. FCBHDBP-257 The volumeLanguageFamily endpoint has been fixed to avoid to get a language v2 record if the language_code paramter is empty. FCBHDBP-261, the version endpoint has been fixed and it returns all available records now.

--- app/Transformers/V2/LibraryVolumeTransformer.php
+++ app/Transformers/V2/LibraryVolumeTransformer.php
@@ -53,11 +53,9 @@ class LibraryVolumeTransformer extends BaseTransformer
 
             case 'v2_library_version':
                 return [
-                    [
-                        'version_code' => substr($fileset->id, 3) ?? '',
-                        'version_name' => $fileset->ver_title,
-                        'english_name' => $fileset->eng_title
-                    ]
+                    'version_code' => substr($fileset->id, 3) ?? '',
+                    'version_name' => $fileset->ver_title,
+                    'english_name' => $fileset->eng_title
                 ];
                 break;
 
