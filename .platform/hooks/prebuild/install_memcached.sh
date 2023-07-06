d57a3e4a3956ae3ba36d0e0dace334be8a5269bf
Brad Flood <bflood@keyholesoftware.com>
2023-07-06 14:57:18 -0500

    Revert "remove installation steps now handled in buildspec"
    This reverts commit 92dfd2a3428f646e6371f873ee464cca6ac7ba4a.

--- .platform/hooks/prebuild/install_memcached.sh
+++ .platform/hooks/prebuild/install_memcached.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 
-# Install igbinary and memcached
-apt-get update
-apt-get install -y php-pear php-dev
-apt-get install -y libmemcached-dev
-pecl install igbinary
-pecl install -D 'enable-memcached-igbinary="yes"' memcached
-echo "extension=\"igbinary.so\""  > $(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/45-igbinary.ini
-echo "extension=memcached.so"  > $(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/50-memcached.ini
+sudo yum install libmemcached-devel -y
+sudo pecl channel-update pecl.php.net
+/usr/bin/yes 'no'| /usr/bin/pecl install igbinary
+/usr/bin/yes 'no'| /usr/bin/pecl install -D 'enable-memcached-igbinary="yes"' memcached 
+    
+#hack..pecl install adds the shared library extensions to to php.ini, but in the wrong order (eg igbinary is after memcached)
+# so comment out the pecl config in php.ini and instead copy the ini files into php.d, which should have the same effect
+sed -i 's/extension="memcached.so"/;extension="memcached.so"/g' /etc/php.ini
+sed -i 's/extension="igbinary.so"/;extension="igbinary.so"/g' /etc/php.ini
+cp .platform/files/etc/php.d/45-igbinary.ini /etc/php.d
+cp .platform/files/etc/php.d/50-memcached.ini /etc/php.d
---
+++ .platform/hooks/prebuild/install_newrelic.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
+yum install newrelic-php5 -y
+cp /usr/lib/newrelic-php5/scripts/newrelic.ini.template /etc/php.d/newrelic.ini
+
+source /var/app/staging/.env
+export NR_INSTALL_SILENT=true;export NR_INSTALL_KEY; newrelic-install install
+echo newrelic.enabled=true  >> /etc/php.d/newrelic.ini
+echo newrelic.loglevel=debug  >> /etc/php.d/newrelic.ini
+
+sed -i 's/newrelic.appname/;newrelic.appname/g' /etc/php.d/newrelic.ini
+echo newrelic.appname=\"$NEW_RELIC_APP_NAME\" >> /etc/php.d/newrelic.ini
