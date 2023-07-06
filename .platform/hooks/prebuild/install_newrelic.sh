3a0b6c0909d744d880911b4f6c6c36ab2ca3ca57
Brad Flood <bflood@keyholesoftware.com>
2023-07-06 14:57:18 -0500

    Revert "remove installation steps now handled in buildspec"
    This reverts commit 92dfd2a3428f646e6371f873ee464cca6ac7ba4a.

--- .platform/hooks/prebuild/install_newrelic.sh
+++ .platform/hooks/prebuild/install_newrelic.sh
@@ -4,13 +4,6 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install newrelic-php5 -y
 cp /usr/lib/newrelic-php5/scripts/newrelic.ini.template /etc/php.d/newrelic.ini
 
-# currently, NewRelic does not provide an install package for ARM64
-# An AMI was created manually based on:
-# 1) the beanstalk platform AMI (ami-0e96552b04cc8cb6d)
-# 2) manually installing NewRelic per these instructions: https://docs.newrelic.com/docs/apm/agents/php-agent/installation/php-agent-installation-arm64/
-#
-
-
 source /var/app/staging/.env
 export NR_INSTALL_SILENT=true;export NR_INSTALL_KEY; newrelic-install install
 echo newrelic.enabled=true  >> /etc/php.d/newrelic.ini
