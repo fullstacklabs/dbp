e49bb3290706fe2c8e67582306d01c27b2e5e610
Jaime Niswonger <jniswonger@keyholesoftware.com>
2023-11-02 16:52:56 -0500

    use original installs for memcached and newrelic

--- .platform/hooks/prebuild/install_memcached.sh
+++ .platform/hooks/prebuild/install_memcached.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 sudo yum install libmemcached-devel -y
 sudo pecl channel-update pecl.php.net
 /usr/bin/yes 'no'| /usr/bin/pecl install igbinary
@@ -12,3 +11,15 @@ sed -i 's/extension="memcached.so"/;extension="memcached.so"/g' /etc/php.ini
 sed -i 's/extension="igbinary.so"/;extension="igbinary.so"/g' /etc/php.ini
 cp .platform/files/etc/php.d/45-igbinary.ini /etc/php.d
 cp .platform/files/etc/php.d/50-memcached.ini /etc/php.d
+
+
+
+# Install igbinary and memcached
+# apt-get update
+# apt-get install -y php-pear php-dev
+# apt-get install -y libmemcached-dev
+# pecl install igbinary
+# pecl install -D 'enable-memcached-igbinary="yes"' memcached
+# echo "extension=\"igbinary.so\""  > $(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/45-igbinary.ini
+# echo "extension=memcached.so"  > $(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/50-memcached.ini
+
