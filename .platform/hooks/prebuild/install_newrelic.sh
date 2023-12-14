c164d90b501b2a3d6893fe9de1005b16d712184d
Jaime Niswonger <jniswonger@keyholesoftware.com>
2023-12-14 15:38:00 -0600

    set license key on install

--- .platform/hooks/prebuild/install_newrelic.sh
+++ .platform/hooks/prebuild/install_newrelic.sh
@@ -9,7 +9,10 @@ set +a
 curl -Ls -o newrelic-php5.tar.gz https://download.newrelic.com/php_agent/archive/10.14.0.3/newrelic-php5-10.14.0.3-linux.tar.gz
 gzip -dc newrelic-php5.tar.gz | tar xf -
 cd newrelic-php5-*
-env NR_INSTALL_SILENT=1 ./newrelic-install install
+export NR_INSTALL_SILENT=1 
+export NR_INSTALL_KEY=$NEW_RELIC_LICENSE_KEY 
+./newrelic-install install
+
 # Clean up
 rm -rf newrelic-php5-*
 rm newrelic-php5.tar.gz
@@ -18,5 +21,4 @@ rm newrelic-php5.tar.gz
 echo extension=newrelic.so | tee /etc/php.d/newrelic.ini
 echo newrelic.enabled=true | tee -a /etc/php.d/newrelic.ini
 echo newrelic.loglevel=debug | tee -a /etc/php.d/newrelic.ini
-echo newrelic.license=\"$NEW_RELIC_LICENSE_KEY\" | tee -a /etc/php.d/newrelic.ini
 echo newrelic.appname=\"$NEW_RELIC_APP_NAME\" | tee -a /etc/php.d/newrelic.ini
